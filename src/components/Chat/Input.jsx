8de724a3d91bc9e570a9e9b828e3ee9f9618a82e
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-04-23 14:27:54 +0700

    Fake App Chat 03

--- src/components/Chat/Input.jsx
+++ src/components/Chat/Input.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import config from "../../config.json";
 const {SERVER_API} = config;
+
 export default function Input() {
   const [form, setForm] = useState({
     user: '',
     message: ''
   });
+  const [currentUser, setCurrentUser] = useState(null);
   const handleSubmit = (e) => {
     e.preventDefault();
     const {user, message} = form;
     if (!user || !message) {
       alert('Vui lòng nhập đầy đủ thông tin');
       return;
+    }
+    if (user !== currentUser) {
+      localStorage.setItem('chat_user', user);
     }
     sendMessage(form);
 
@@ -33,11 +38,17 @@ export default function Input() {
     }
     setForm({...form, message: ''});
   }
+  useEffect(() => {
+    if (localStorage.getItem('chat_user') ) {
+      setCurrentUser(localStorage.getItem('chat_user'));
+      setForm({...form, user: localStorage.getItem('chat_user')})
+    }
+  }, []);
   return (
     <form className="mt-3" onSubmit={handleSubmit}>
         <div className="mb-3">
             <input type="text" className="form-control" placeholder="Tên của bạn..." name="user"
-             onChange={handleChange}/>
+             onChange={handleChange} defaultValue={currentUser}/>
         </div>
         <div className="input-group">
             <input type="text" className="form-control" placeholder="Tin nhắn..." name="message"  onChange={handleChange} value={form.message}/>
--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -3,16 +3,24 @@ import config from "../../config.json";
 const {SERVER_API} = config;
 export default function Message() {
   const [messages, setMessages] = useState([]);
+  const scrollBottom = () => {
+    const messagesEl = document.querySelector('.messages');
+    const messageHeight = messagesEl.scrollHeight;
+    messagesEl.scroll(0, messageHeight);
+  }
   const getMessages = async () => {
     const response = await fetch(`${SERVER_API}/messages`);
     const data = await response.json();
     setMessages(data);
+    scrollBottom();
   }
   useEffect(() => {
     setTimeout(() => {
         getMessages();
     }, 1000);
   }, [messages]);
+
+  
   return (
     <div className="messages border p-3" style={{
         height: '300px',
